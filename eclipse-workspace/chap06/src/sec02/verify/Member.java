f390cfd915aa3ee44685b874c21371764f2fc559
김진광 <113023365+kjink98@users.noreply.github.com>
2023-09-25 18:15:56 +0900

    06-2 필드

---
+++ eclipse-workspace/chap06/src/sec02/verify/Member.java
@@ -0,0 +1,8 @@
+package sec02.verify;
+
+public class Member {
+	String name = "";
+	String id = "";
+	String password = "";
+	int age = 0;
+}
---
+++ eclipse-workspace/chap06/src/sec02/verify/MemberExample.java
@@ -0,0 +1,13 @@
+package sec02.verify;
+
+public class MemberExample {
+
+	public static void main(String[] args) {
+		// TODO P231 - 3
+		Member member = new Member();
+		member.name = "최하얀";
+		member.age = 23;
+		
+	}
+
+}
