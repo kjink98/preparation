e1197d6c482f35733719b366db45eca2398125e6
김진광 <113023365+kjink98@users.noreply.github.com>
2023-09-26 23:21:51 +0900

    06 - 3 생성자

---
+++ eclipse-workspace/chap06/src/sec03/exam01/CarExample.java
@@ -0,0 +1,12 @@
+package sec03.exam01;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		// TODO 생성자를 호출해서 객체 생성
+		Car myCar = new Car("검정", 3000);
+		// Car myCar = new Car(); --> 기본 생성자를 호출할 수 없음
+
+	}
+
+}
