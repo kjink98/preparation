de6c9f150c25bff92f5e2568a0f2e6c07de1bf12
김진광 <113023365+kjink98@users.noreply.github.com>
2023-10-12 23:27:05 +0900

    내부에서 메소드 호출

---
+++ eclipse-workspace/chap06/src/sec04/exam04/CalculatorExample.java
@@ -0,0 +1,12 @@
+package sec04.exam04;
+
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+		// TODO Calculator의 excute() 실행
+		Calculator myCalc = new Calculator();
+		myCalc.excute();
+
+	}
+
+}
