983c3a0116fa6f6217cec989004a3e5abeb6424a
김진광 <113023365+kjink98@users.noreply.github.com>
2023-10-15 16:42:38 +0900

    메소드 문제, 인스턴스 멤버, 정적 멤버

---
+++ eclipse-workspace/chap06/src/sec04/verify/Printer.java
@@ -0,0 +1,19 @@
+package sec04.verify;
+
+public class Printer {
+	void println(int a) {
+		System.out.println(a);
+	}
+	
+	void println(boolean a) {
+		System.out.println(a);
+	}
+	
+	void println(double a) {
+		System.out.println(a);
+	}
+	
+	void println(String a) {
+		System.out.println(a);
+	}
+}
---
+++ eclipse-workspace/chap06/src/sec05/exam01/CarExample.java
@@ -0,0 +1,15 @@
+package sec05.exam01;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		// TODO 인스턴스 멤버와 this
+		Car myCar = new Car("포르쉐");
+		Car yourCar = new Car("벤츠");
+
+		myCar.run();
+		yourCar.run();
+		
+	}
+
+}
