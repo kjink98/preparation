f38d831dc973c0dccc9c0510c038d5e956cddbfc
김진광 <113023365+kjink98@users.noreply.github.com>
2023-10-23 17:52:03 +0900

    상속

---
+++ eclipse-workspace/chap07/src/sec01/exam03/Calculator.java
@@ -0,0 +1,8 @@
+package sec01.exam03;
+
+public class Calculator {
+	double areaCircle(double r) {
+		System.out.println("Calculator 객체의 areaCircle() 실행");
+		return 3.14159 * r * r; // 정밀한 계산을 위해 수정
+	}
+}
---
+++ eclipse-workspace/chap07/src/sec01/exam05/VeryImportantkPerson.java
@@ -0,0 +1,5 @@
+package sec01.exam05;
+
+//public class VeryImportantkPerson extends Members {
+public class VeryImportantkPerson {
+}
