89250c39df59202686d4fa446747defa7109dfed
kjink98 <kjink98@naver.com>
2023-11-11 15:53:17 +0900

    익명 객체

--- eclipse-workspace/chap09/src/sec01/exam01/A.java
+++ eclipse-workspace/chap09/src/sec01/exam01/A.java
@@ -27,9 +27,9 @@ class A {
 		class D {
 			D() {System.out.println("D 객체가 생성됨"); }
 			int field1;
-			static int field2;
+//			static int field2;
 			void method1() { }
-			static void method2() { }
+//			static void method2() { }
 		}
 		D d = new D();
 		d.field1 = 3;
